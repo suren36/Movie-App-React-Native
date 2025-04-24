64c0f56dffefac47b3842ba1bfc5abca588f2146
suren36 <79780614+suren36@users.noreply.github.com>
2025-04-24 17:10:02 +0545

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,80 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { View, Text, ImageBackground, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+const tabs = [
+    { name: "index", title: "Home", icon: icons.home },
+    { name: "search", title: "Search", icon: icons.search },
+    { name: "saved", title: "Saved", icon: icons.save },
+    { name: "profile", title: "Profile", icon: icons.person },
+  ];
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const TabIcon = ({focused,icon,title}:any) => {
+    if(focused){
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+        return (
+            <ImageBackground
+            source={images.highlight}
+            className="flex flex-row w-full flex-1 min-w-[112px]  min-h-16  mt-4 justify-center items-center rounded-full overflow-hidden"
+            >
+            <Image source={icon} className="size-5" tintColor="#FFFFFF" />
+            <Text className="text-seconday text-base font-semibold ml-2 text-white">{title}</Text>
+            </ImageBackground>
+        );
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    }
+    else
+    {
+        return (
+            <View className="size-full justify-center items-center mt-4 rounded-full overflow-hidden">
+            <Image source={icon} tintColor="#A8B5DB" />
+            </View>
+        );
+
+    }
+
 }
+const _layout = () => {
+  return (
+    <Tabs screenOptions = {{
+        tabBarShowLabel: false,
+        tabBarItemStyle :{
+            width :'100%',
+            height:"100%",
+            justifyContent:'center',
+            alignItems:'center',
+          
+        },
+        tabBarStyle :{
+            backgroundColor:"#0F0D23",
+            borderRadius:50,
+            marginHorizontal:20,
+            marginBottom:36,
+            height:52,
+            overflow:'hidden',
+            position:'absolute',
+            borderWidth:1,
+            borderColor:'0f0d23',
+   
+
+        }
+    }}>
+    {tabs.map(({ name, title, icon }) => (
+      <Tabs.Screen
+        key={name}
+        name={name}
+        options={{
+          title,
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icon} title={title} />
+          ),
+        }}
+      />
+    ))}
+  </Tabs>
+  );
+};
+
+export default _layout;
