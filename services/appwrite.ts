eda19057f0c2acb58cbbd56323f225cebbe87aef
suren36 <79780614+suren36@users.noreply.github.com>
2025-05-12 16:57:49 +0545

    updated search count  in appwrite database

---
+++ services/appwrite.ts
@@ -0,0 +1,65 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+// track the searches made by a user
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+ 
+try
+{
+
+  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    Query.equal("searchTerm", query),
+  ]);
+  console.log(result);
+
+  if (result.documents.length > 0) {
+    const existingMovie = result.documents[0];
+
+    await database.updateDocument(
+      DATABASE_ID,
+      COLLECTION_ID,
+      existingMovie.$id,
+      {
+        count: existingMovie.count + 1,
+      }
+    );
+  }
+  else{
+
+    
+    await database.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {
+            title: movie.title,
+            searchTerm : query,
+            movie_id: movie.id,
+            count:1,
+           poster_url:`https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        }
+         
+
+    )
+
+
+  }
+
+}
+
+catch (error) {
+    console.error("Error updating search count:", error);
+    throw error; // Rethrow the error to be handled by the calling function
+    // Handle the error here (e.g., show a notification, log it, etc.)
+
+}
+  // Handle the result here
+};
